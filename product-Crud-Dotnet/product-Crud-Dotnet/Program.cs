dc66d8332854b66e64c735950748d4552dbefbdf
kithekadk <dankinyi99@gmail.com>
2024-05-04 17:48:09 +0300

    testing endpoints

--- product-Crud-Dotnet/product-Crud-Dotnet/Program.cs
+++ product-Crud-Dotnet/product-Crud-Dotnet/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<ProductDbContext>(options =>
 });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
