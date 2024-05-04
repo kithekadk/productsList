611777bd703d675eeaeadd28f1acd716a4a10ed6
kithekadk <dankinyi99@gmail.com>
2024-05-04 23:33:59 +0300

    Set default id

--- product-Crud-Dotnet/product-Crud-Dotnet/Models/Entities/Product.cs
+++ product-Crud-Dotnet/product-Crud-Dotnet/Models/Entities/Product.cs
@@ -2,7 +2,7 @@
 {
     public class Product
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public int Price { get; set; }
